40f3835ad49a97e111d00894fa28d15e0bf568f4
paulallton <paulallton@gmail.com>
2012-05-22 09:17:01 +0000

    added bash.bat

---
+++ bash.bat
@@ -0,0 +1,7 @@
+rem ### magic to make cygwin use the current directory for its working directory, see: http://superuser.com/questions/345964/start-bash-shell-cygwin-with-correct-path-without-changing-directory
+set CHERE_INVOKED=1
+rem ### replace all the "\" into "/" before passing the current directory to cygwin
+set cwd=%CD%
+set cwd=%cwd:\=/%
+cls
+\cygwin\bin\bash.exe -l -c "cd %cwd%;./%1 %2"
